39174462f5bb6a053b04242e85173d2ab2dd1b06
Rene Eizenhoefer <Rene.Eizenhoefer@ubl-is.de>
2020-04-27 12:03:36 +0200

    fixed wrong server, formatted documents, removed blank lines...

--- Private/Get-NtpTransactionData.ps1
+++ Private/Get-NtpTransactionData.ps1
@@ -1,5 +1,6 @@
-function Get-NtpTransactionData {
-<#
+function Get-NtpTransactionData
+{
+    <#
 .SYNOPSIS
      Get NTP Tansactio Data, including Ntp raw binary data and transaction start and end time.
 .DESCRIPTION
@@ -9,26 +10,26 @@ function Get-NtpTransactionData {
     The NTP time packet from the server is decoded and returned.
 
     Note: this uses NTP (rfc-1305: http://www.faqs.org/rfcs/rfc1305.html) on UDP 123.  Because the
-    function makes a single call to a single server this is strictly a SNTP client (rfc-2030),  
+    function makes a single call to a single server this is strictly a SNTP client (rfc-2030),
     although the SNTP protocol data is similar (and can be identical) and the clients and servers
-    are often unable to distinguish the difference.  Where SNTP differs is that is does not 
+    are often unable to distinguish the difference.  Where SNTP differs is that is does not
     accumulate historical data (to enable statistical averaging) and does not retain a session
     between client and server.
 
-    An alternative to NTP or SNTP is to use Daytime (rfc-867) on TCP port 13 - although this is an 
-    old protocol and is not supported by all NTP servers.  This NTP function will be more accurate than 
-    Daytime (since it takes network delays into account) but the result is only ever based on a 
-    single sample.  Depending on the source server and network conditions the actual returned time 
+    An alternative to NTP or SNTP is to use Daytime (rfc-867) on TCP port 13 - although this is an
+    old protocol and is not supported by all NTP servers.  This NTP function will be more accurate than
+    Daytime (since it takes network delays into account) but the result is only ever based on a
+    single sample.  Depending on the source server and network conditions the actual returned time
     may not be as accurate as required.
 
     See rfc-2030.md extract of the SNTP rfc.
-    
+
     Script Operation, Detail:
 
     Construct an NTP request packet
     Record the current local time; This is time t1, the 'Originate Timestamp'
     Send the NTP request packet to the selected server
-    Read the server response 
+    Read the server response
     Record the current local time after reception.  This is time t4.
 
     The received packet now contains:
@@ -39,23 +40,23 @@ function Get-NtpTransactionData {
 
     Calculate clock offset and delay:
 
-    Estimated Clock Offset 
+    Estimated Clock Offset
     This is the difference between the server clock and the local clock taking into account
-    the network latency.  If both server and client clocks have the same absolute time 
+    the network latency.  If both server and client clocks have the same absolute time
     then the clock difference minus the network latency will be 0.
 
-    Assuming symetric send/receive delays, the average of the out and return times will 
+    Assuming symetric send/receive delays, the average of the out and return times will
     equal the offset.
 
     Offset = (OutTime+ReturnTime)/2
 
-    Offset = ((t2 - t1) + (t3 - t4))/2      
+    Offset = ((t2 - t1) + (t3 - t4))/2
 
     Adding the offset to the local clock will give the correct time.
 
 
     Round Trip Delay (= the time actually spent on the network)
-    This is the total transaction time (between t1..t4) minus the server 'thinking 
+    This is the total transaction time (between t1..t4) minus the server 'thinking
     time' (between t2..t3)
 
     Delay = (t4 - t1) - (t3 - t2)
@@ -68,7 +69,7 @@ function Get-NtpTransactionData {
 .PARAMETER Server
 
     The NTP Server to contact.  Uses pool.ntp.org by default.
-.EXAMPLE 
+.EXAMPLE
     Get-NtpTransactionData
 
     Get Ntp transaction dtat for localhost
@@ -77,129 +78,150 @@ function Get-NtpTransactionData {
 .OUTPUTS
    Returns true if all computerNames are within the specified offset time, if not returns false.
 #>
-[cmdletbinding()]
-param(
-    [string[]]$computerName = $($env:computerName), 
-    [string]$server = 'pool.ntp.org'
-)
-    try {
-        $ipAddressOfNtpServer = Resolve-DnsName 'ntp.oakland.edu'| where-object {$_.section -eq "Answer"} | select -ExpandProperty ipAddress | select -first 1 
-    } catch {
-        write-warning "Unable to perform a reverse lookup of $server"
+    [cmdletbinding()]
+    param(
+        [string[]]$computerName = $($env:computerName),
+        [string]$server = 'pool.ntp.org'
+    )
+    try
+    {
+        $ipAddressOfNtpServer = Resolve-DnsName $server | Where-Object { $_.section -eq "Answer" } | Select-Object -ExpandProperty ipAddress | Select-Object -first 1
+    }
+    catch
+    {
+        Write-Warning "Unable to perform a reverse lookup of $server"
     }
 
 
-    foreach ($computer in $computerName){
+    foreach ($computer in $computerName)
+    {
 
         # Construct a 48-byte client NTP time packet to send to the specified server
-        [Byte[]]$NtpData = ,0 * 48
+        [Byte[]]$NtpData = , 0 * 48
 
         # (Construct Request Header: [00=No Leap Warning; 011=Version 3; 011=Client Mode]; 00011011 = 0x1B)
-        $NtpData[0] = 0x1B    # NTP Request header in first byte  
+        $NtpData[0] = 0x1B    # NTP Request header in first byte
 
-        if ($computer -eq $($env:computerName)){
+        if ($computer -eq $($env:computerName))
+        {
 
             $Socket = New-Object -TypeName Net.Sockets.Socket -ArgumentList ([Net.Sockets.AddressFamily]::InterNetwork,
-                                                                            [Net.Sockets.SocketType]::Dgram,
-                                                                            [Net.Sockets.ProtocolType]::Udp)
+                [Net.Sockets.SocketType]::Dgram,
+                [Net.Sockets.ProtocolType]::Udp)
             $Socket.SendTimeOut = 2000  # ms
             $Socket.ReceiveTimeOut = 2000   # ms
 
-            Try {
-                $Socket.Connect($Server,123)
+            Try
+            {
+                $Socket.Connect($Server, 123)
             }
-            Catch {
-                #try IP address before failing for 
-                Try{
-                    $Socket.Connect($ipAddressOfNtpServer,123)
-                } Catch {
+            Catch
+            {
+                #try IP address before failing for
+                Try
+                {
+                    $Socket.Connect($ipAddressOfNtpServer, 123)
+                }
+                Catch
+                {
                     Write-Error -Message "Failed to connect to server $Server"
-                    Throw 
+                    Throw
                 }
             }
 
-        # NTP Transaction -------------------------------------------------------
-                $t1 = Get-Date    # t1, = Start time of transaction... 
-                Try {
+            # NTP Transaction -------------------------------------------------------
+            $t1 = Get-Date    # t1, = Start time of transaction...
+            Try
+            {
+                [Void]$Socket.Send($NtpData)      # Send request header
+                [Void]$Socket.Receive($NtpData)   # Receive 48-byte NTP response
+            }
+            Catch
+            {
+                Write-Error -Message "Failed to communicate with server $Server"
+                Throw
+            }
+            $t4 = Get-Date    # t4, = End of NTP transaction time
+            # End of NTP Transaction ------------------------------------------------
+
+            $Socket.Shutdown('Both')
+            $Socket.Close()
+
+            $properties = @{computerName = $computer
+                Status                   = 'Success'
+                NtpData                  = $NtpData
+                TransactionStart         = $t1
+                TransactionEnd           = $t4
+            }
+
+            $obj = New-Object -TypeName PSObject -Property $properties
+            $obj.psobject.typenames.insert(0, 'ntpData.Object')
+            Write-Output $obj
+
+        }
+        else
+        {
+
+            $ntpTransactionData = Invoke-Command -ComputerName $computer -scriptblock {
+                param($ipAddressOfNtpServer, $Server, $NtpData)
+
+                $Socket = New-Object -TypeName Net.Sockets.Socket -ArgumentList ([Net.Sockets.AddressFamily]::InterNetwork,
+                    [Net.Sockets.SocketType]::Dgram,
+                    [Net.Sockets.ProtocolType]::Udp)
+                $Socket.SendTimeOut = 2000  # ms
+                $Socket.ReceiveTimeOut = 2000   # ms
+
+                Try
+                {
+                    $Socket.Connect($Server, 123)
+                }
+                Catch
+                {
+                    #try IP address before failing for
+                    Try
+                    {
+                        $Socket.Connect($ipAddressOfNtpServer, 123)
+                    }
+                    Catch
+                    {
+                        Write-Error -Message "Failed to connect to server $Server"
+                        Throw
+                    }
+                }
+
+                # NTP Transaction -------------------------------------------------------
+                $t1 = Get-Date    # t1, = Start time of transaction...
+                Try
+                {
                     [Void]$Socket.Send($NtpData)      # Send request header
                     [Void]$Socket.Receive($NtpData)   # Receive 48-byte NTP response
                 }
-                Catch {
+                Catch
+                {
                     Write-Error -Message "Failed to communicate with server $Server"
                     Throw
                 }
                 $t4 = Get-Date    # t4, = End of NTP transaction time
-        # End of NTP Transaction ------------------------------------------------
+                # End of NTP Transaction ------------------------------------------------
 
-            $Socket.Shutdown('Both') 
-            $Socket.Close()
-
-            $properties = @{computerName = $computer
-                Status = 'Success'
-                NtpData = $NtpData
-                TransactionStart = $t1
-                TransactionEnd =  $t4
-                }  
-
-            $obj = New-Object -TypeName PSObject -Property $properties
-            $obj.psobject.typenames.insert(0,'ntpData.Object')
-            Write-Output $obj 
-
-        } else {
-
-            $ntpTransactionData = invoke-command -ComputerName $computer -scriptblock {    
-            param($ipAddressOfNtpServer, $Server, $NtpData)
-                
-                $Socket = New-Object -TypeName Net.Sockets.Socket -ArgumentList ([Net.Sockets.AddressFamily]::InterNetwork,
-                                                                        [Net.Sockets.SocketType]::Dgram,
-                                                                        [Net.Sockets.ProtocolType]::Udp)
-                $Socket.SendTimeOut = 2000  # ms
-                $Socket.ReceiveTimeOut = 2000   # ms
-
-                Try {
-                    $Socket.Connect($Server,123)
-                }
-                Catch {
-                    #try IP address before failing for 
-                    Try{
-                        $Socket.Connect($ipAddressOfNtpServer,123)
-                    } Catch {
-                        Write-Error -Message "Failed to connect to server $Server"
-                        Throw 
-                    }
-                }
-
-            # NTP Transaction -------------------------------------------------------
-                    $t1 = Get-Date    # t1, = Start time of transaction... 
-                    Try {
-                        [Void]$Socket.Send($NtpData)      # Send request header
-                        [Void]$Socket.Receive($NtpData)   # Receive 48-byte NTP response
-                    }
-                    Catch {
-                        Write-Error -Message "Failed to communicate with server $Server"
-                        Throw
-                    }
-                    $t4 = Get-Date    # t4, = End of NTP transaction time
-            # End of NTP Transaction ------------------------------------------------
-
-                $Socket.Shutdown('Both') 
+                $Socket.Shutdown('Both')
                 $Socket.Close()
 
                 $properties = @{computerName = $env:computerName
-                    Status = 'Success'
-                    NtpData = $NtpData
-                    TransactionStart = $t1
-                    TransactionEnd =  $t4
-                    }  
+                    Status                   = 'Success'
+                    NtpData                  = $NtpData
+                    TransactionStart         = $t1
+                    TransactionEnd           = $t4
+                }
 
                 $obj = New-Object -TypeName PSObject -Property $properties
-                $obj.psobject.typenames.insert(0,'ntpData.Object')
-                Write-Output $obj 
+                $obj.psobject.typenames.insert(0, 'ntpData.Object')
+                Write-Output $obj
 
-            } -ArgumentList $ipAddressOfNtpServer, $Server, $NtpData  
+            } -ArgumentList $ipAddressOfNtpServer, $Server, $NtpData
 
-            write-output $ntpTransactionData
-        
+            Write-Output $ntpTransactionData
+
         }
     }
 }
--- Public/Test-NtpTime.ps1
+++ Public/Test-NtpTime.ps1
@@ -1,5 +1,6 @@
-Function Test-NtpTime {
-<#
+Function Test-NtpTime
+{
+    <#
 .SYNOPSIS
     Tests Network Time Protocol time (SNTP/NTP, rfc-1305, rfc-2030) from a specified server is within specified Offset.
 .DESCRIPTION
@@ -13,12 +14,12 @@ Function Test-NtpTime {
 .PARAMETER NoDns
    (Switch) If specified do not attempt to resolve Version 3 Secondary Server ReferenceIdentifiers.
 .PARAMETER useConfiguredTimeServer
-   (Switch) If specified use whatever ntp is configured on the computer. 
+   (Switch) If specified use whatever ntp is configured on the computer.
 .EXAMPLE
    test-NtpTime uk.pool.ntp.org
    Determins if the local computer's time is in sync with uk.pool.ntp.org within at most 10000 milliseconds.
 .EXAMPLE
-   Test-ntpTime -computerName pc1, pc2 , pc3 -noDns  
+   Test-ntpTime -computerName pc1, pc2 , pc3 -noDns
    Determins if computers pc1, pc2, and pc3 are all in sync with pool.ntp.org within at most 10000 milliseconds.
 .EXAMPLE
      Test-ntpTime -computername PC01 -useConfiguredTimeServer -MaxOffset 10
@@ -29,31 +30,38 @@ Function Test-NtpTime {
 #>
 
 
-[cmdletbinding()]
-param(
-    [string[]]$computerName = $($env:computerName),
-    [String]$Server = 'pool.ntp.org',
-    [Int]$MaxOffset = 10000,     # (Milliseconds) Throw exception if network time offset is larger
-    [Switch]$NoDns,               # Do not attempt to lookup V3 secondary-server referenceIdentifier
-    [Switch]$useConfiguredTimeServer
-)
+    [cmdletbinding()]
+    param(
+        [string[]]$computerName = $($env:computerName),
+        [String]$Server = 'pool.ntp.org',
+        [Int]$MaxOffset = 10000, # (Milliseconds) Throw exception if network time offset is larger
+        [Switch]$NoDns, # Do not attempt to lookup V3 secondary-server referenceIdentifier
+        [Switch]$useConfiguredTimeServer
+    )
     $offsetExceeded = $false
-    foreach ($computer in $computerName){
-        write-verbose "flag value is $useConfiguredTimeServer"
+    foreach ($computer in $computerName)
+    {
+        Write-Verbose "flag value is $useConfiguredTimeServer"
         $ntpResults = get-ntpTime -Server $Server -MaxOffset $MaxOffset -NoDns:$noDns -useConfiguredTimeServer:$useConfiguredTimeServer -computerName $computerName -MaxOffsetErrorAction silentlyContinue
-        if ($ntpResults.offset -gt $maxOffset){
-            write-verbose "[Failed] Actual Offset of $($ntpResults.offset) exceeds Max off set of $maxOFfset Milliseconds on $computer"
+        if ($ntpResults.offset -gt $maxOffset)
+        {
+            Write-Verbose "[Failed] Actual Offset of $($ntpResults.offset) exceeds Max off set of $maxOFfset Milliseconds on $computer"
             $offsetExceeded = $true
-        } else {
-            write-verbose "[Success] Actual Offset of $($ntpResults.offset) does not exceed Max off set of $maxOFfset Milliseconds on $computer"
+        }
+        else
+        {
+            Write-Verbose "[Success] Actual Offset of $($ntpResults.offset) does not exceed Max off set of $maxOFfset Milliseconds on $computer"
         }
     }
-    if ($offsetExceeded){
-        write-verbose "[Failed] Offset of at least one host exceed Max off set of $maxOFfset Milliseconds."
-        write-Output $false
-    } else {
-        write-verbose "[Success] Offset of all hosts tested do not exceed Max off set of $maxOFfset Milliseconds."
-         Write-Output $true
+    if ($offsetExceeded)
+    {
+        Write-Verbose "[Failed] Offset of at least one host exceed Max off set of $maxOFfset Milliseconds."
+        Write-Output $false
+    }
+    else
+    {
+        Write-Verbose "[Success] Offset of all hosts tested do not exceed Max off set of $maxOFfset Milliseconds."
+        Write-Output $true
     }
 
 
